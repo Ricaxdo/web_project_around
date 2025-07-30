1b6e0389383ddca895a542538473c18a95e45e95
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-07-30 04:40:59 -0600

    Change of name editPopup.css (html, css, js)

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
 let editButton = document.querySelector(".profile__edit-icon");
-let popup = document.querySelector(".popup");
-let popupCloseButton = document.querySelector(".popup__close-button-icon");
-let popupSubmitButton = document.querySelector(".popup__submit-button");
+let editPopup = document.querySelector(".editPopup");
+let editPopupCloseButton = document.querySelector(
+  ".editPopup__close-button-icon"
+);
+let editPopupSubmitButton = document.querySelector(".editPopup__submit-button");
 
-let inputName = document.querySelector(".popup__input-name");
-let inputAbout = document.querySelector(".popup__input-about");
+let inputName = document.querySelector(".editPopup__input-name");
+let inputAbout = document.querySelector(".editPopup__input-about");
 
 let infoName = document.querySelector(".profile__info-name");
 let infoAbout = document.querySelector(".profile__info-description");
 
-function openPopup() {
-  popup.classList.remove("popup_unshown");
+function openEditPopup() {
+  editPopup.classList.remove("editPopup_unshown");
   inputName.value = infoName.textContent;
   inputAbout.value = infoAbout.textContent;
 }
 
-function closePopup() {
-  popup.classList.add("popup_unshown");
+function closeEditPopup() {
+  editPopup.classList.add("editPopup_unshown");
 }
 
 function saveProfile(evt) {
   evt.preventDefault();
   infoName.textContent = inputName.value;
   infoAbout.textContent = inputAbout.value;
-  closePopup();
+  closeEditPopup();
 }
 
-editButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
-popupSubmitButton.addEventListener("click", saveProfile);
+editButton.addEventListener("click", openEditPopup);
+editPopupCloseButton.addEventListener("click", closeEditPopup);
+editPopupSubmitButton.addEventListener("click", saveProfile);
